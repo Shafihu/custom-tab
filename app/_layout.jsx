396bda9275d7579f6d6dbd110051da901c38b4fc
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-09-09 06:50:29 +0000

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+
+const _layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default _layout;
